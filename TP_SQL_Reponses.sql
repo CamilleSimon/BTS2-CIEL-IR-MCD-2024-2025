ee5ced684cbfcfbf0be53bd41c2a645f440bdf7d
BessyM <bessymartin12@gmail.com>
2024-10-15 16:04:08 +0200

    Update TP_SQL_Reponses.sql

--- TP_SQL_Reponses.sql
+++ TP_SQL_Reponses.sql
@@ -1,1 +1,9 @@
-
+// Réponse question 1 :
+CREATE TABLE champions(
+  champion_id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50),
+  lore VARCHAR(50),
+  FOREIGN KEY (gender_id) REFERENCES gender(gender_id),
+  FOREIGN KEY (resource_id) REFERENCES resource(resource_id),
+  FOREIGN KEY (year_id) REFERENCES year(year_id),
+  
